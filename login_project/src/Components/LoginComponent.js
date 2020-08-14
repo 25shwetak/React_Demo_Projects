381e99672f256649cc24d5c5be8fb9693d0b0f23
25shwetak <25shwetakshasha.com>
2020-08-14 19:37:54 +0530

    validations added

--- login_project/src/Components/LoginComponent.js
+++ login_project/src/Components/LoginComponent.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { Button, Form, FormGroup, Label, Input, Row, Col, Container, Card, CardBody} from 'reactstrap';
+import { Button, Form, FormGroup, FormFeedback, Label, Input, Row, Col, Container, Card, CardBody} from 'reactstrap';
 import PropTypes from 'prop-types';
 
 const LoginComponent = (props) => {
-  let {username, password, setUsername, setPassword, logValues, schema} = props;
+  let {username, password, setUsername, setPassword, schema, 
+    validateData, usernameError, passwordError} = props;
   const setUsernameWrapper = (val) => {
     setUsername(val.target.value)
   }
@@ -21,17 +22,29 @@ const LoginComponent = (props) => {
               <Form>
                 <FormGroup>
                   <Label for="exampleEmail">Email</Label>
-                  <Input type="email" name="email" id="exampleEmail" placeholder="enter your email" value={username} onChange={setUsernameWrapper}/>
+                  <Input 
+                        type="email" name="email" 
+                        id="exampleEmail" 
+                        placeholder="enter your email" 
+                        value={username} 
+                        onChange={setUsernameWrapper}
+                        invalid={(usernameError !== null ? true : false)}/>
+                  <FormFeedback>{usernameError}</FormFeedback>
                 </FormGroup>
                 <FormGroup>
                   <Label for="examplePassword">Password</Label>
-                  <Input type="password" name="password" id="examplePassword" placeholder="enter your password" value={password} onChange={setPasswordWrapper}/>
+                  <Input 
+                        type="password" 
+                        name="password" 
+                        id="examplePassword" 
+                        placeholder="enter your password" 
+                        value={password} 
+                        onChange={setPasswordWrapper}
+                        invalid={(passwordError !== null) ? true : false}/>
+                  <FormFeedback>{passwordError}</FormFeedback>
                 </FormGroup>
               </Form>
-              <Button onClick={() => {
-                                logValues(); 
-                                schema.validate({username: username, password: password}, {abortEarly: false});
-                            }}>Login</Button>
+              <Button onClick={validateData}>Login</Button>
             </CardBody>
           </Card>
         </Col>
@@ -46,8 +59,7 @@ LoginComponent.propTypes = {
   username: PropTypes.string.isRequired,
   password: PropTypes.string.isRequired,
   setUsername: PropTypes.func.isRequired,
-  setPassword: PropTypes.func.isRequired,
-  logValues: PropTypes.func.isRequired
+  setPassword: PropTypes.func.isRequired
 }
 
 LoginComponent.defaultProps = {
--- login_project/src/Containers/LoginContainer.js
+++ login_project/src/Containers/LoginContainer.js
@@ -1,24 +1,52 @@
 import React, { useState } from "react";
 import LoginComponent from "../Components/LoginComponent";
 import * as yup from "yup";
+import apiHelper from "../apis/apiHelper";
 
 const LoginContainer = () => {
 
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [usernameError, setUsernameError] = useState(null)
+  const [passwordError, setPasswordError] = useState(null)
 
-  const logValues = () => {
+  /*const logValues = () => {
     console.log(username);
     console.log(password);
-  }
+  }*/
 
   const schema = yup.object().shape({
         username: yup.string().email().required(),
-        password: yup.string().required(),
+        password: yup.string().min(6).required(),
       });
 
+  const validateData = () => {
+    schema.validate({username, password}, {abortEarly: false})
+    .then((data) => {
+      apiHelper('post', 'https://api.taiga.io/api/v1/auth', {username, password})
+      .then((response) => {
+        console.log(response);
+      })
+    })
+    .catch((err) => {
+      err.inner.forEach((ele) => {
+        if (ele.path === 'username') setUsernameError(ele.message);
+        if (ele.path === 'password') setPasswordError(ele.message);
+        console.log(ele.path, ele.message);
+      });
+    });
+  }
+
   return (
-    <LoginComponent username={username} password={password} setUsername={setUsername} setPassword={setPassword} logValues={logValues} schema={schema}/>
+    <LoginComponent 
+      username={username} 
+      password={password} 
+      setUsername={setUsername} 
+      setPassword={setPassword} 
+      schema={schema}
+      validateData={validateData}
+      usernameError={usernameError}
+      passwordError={passwordError}/>
   )
 }
 
